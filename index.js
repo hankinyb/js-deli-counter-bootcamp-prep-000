5bd262f55ddaef8e3bce917c49ac3116121c5e3f
Yan Hankin <hankinyb@gmail.com>
2018-02-17 20:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function nowServing(katzDeli) {
     return (`Currently serving ${shifted}.`);
   }
 }
+
+function currentLine(katzDeli) {
+  if (katzDeli.length === 0) {
+    return (`The line is currently empty.`);
+  } 
+  else {
+    
+}
