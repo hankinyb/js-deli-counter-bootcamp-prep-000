dfd27b879b4f35822f05a4178857a38c9544e93a
Yan Hankin <hankinyb@gmail.com>
2018-02-17 19:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function takeANumber(line, newPerson) {
   var katzDeli = [];
   katzDeli = line.push(newPerson);
   var i;
-  for (i = 0; i < katzDeli.length; i++) {
-    
+  for (i = 1; i < katzDeli.length; i++) {
+    return "Welcome `$(newPerson)`. You are number [i] in line."
   }
   
   return katzDeli
