b1e09630e528b344095317542c2af75a9093ebca
Yan Hankin <hankinyb@gmail.com>
2018-02-17 19:50:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function takeANumber(line, newPerson) {
-  var katzDeli = [];
-  katzDeli.push(newPerson);
+   katzDeli.push(newPerson);
     return (`Welcome, ${newPerson}. You are number ${katzDeli.length} in line.`);
   
 }
